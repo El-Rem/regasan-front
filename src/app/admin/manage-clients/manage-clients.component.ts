0fe2e15a853811e80db8a7b5e27d8c03fca3b36c
dannielgloria <danniel.gloria@gmail.com>
2025-06-03 20:08:45 -0600

    feat: se actualiza el formulario para actualizacion

--- src/app/admin/manage-clients/manage-clients.component.ts
+++ src/app/admin/manage-clients/manage-clients.component.ts
@@ -29,6 +29,7 @@ export class ManageClientsComponent {
       contact_last_name_2: ['']
     });
 
+
     this.updateForm = this.fb.group({
       rfc: ['', [Validators.required, Validators.pattern('^[A-Z0-9]{12,13}$')]],
       business_name: [{ value: '', disabled: false }, Validators.required],
